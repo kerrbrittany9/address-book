40cfb43dc4ce217f7a6a8ab80ede55200e146302
Brittany Kerr and Nicole Freed <brittany@>
2017-06-12 09:43:17 -0700

    add HTML, JS, and JQuery for address, phone, and DOB

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 //business logic
-function Contact(first, last) {
+function Contact(first, last, address, phone, dob) {
   this.firstName = first;
   this.lastName = last;
+  this.address = address;
+  this.phone = phone;
+  this.dob = dob;
 };
 
 Contact.prototype.fullName = function() {
@@ -15,8 +18,11 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
+    var inputtedAddress = $("input#new-address").val();
+    var inputtedPhone = $("input#new-phone").val();
+    var inputtedDob = $("#new-dob").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedAddress, inputtedPhone, inputtedDob);
     console.log(newContact);
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
@@ -26,9 +32,15 @@ $(document).ready(function() {
     $("#show-contact h2").text(newContact.firstName);
     $(".first-name").text(newContact.firstName);
     $(".last-name").text(newContact.lastName);
+    $(".address").text(newContact.address);
+    $(".phone").text(newContact.phone);
+    $(".dob").text(newContact.dob);
   });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input#new-address").val("");
+    $("input#new-phone").val("");
+    $("input#new-dob").val("");
   });
 });
